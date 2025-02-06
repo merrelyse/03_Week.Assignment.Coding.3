55c5cc41af742f10dbd2b3b2099bfcb0b30b953e
Merritt Leidy <merritt.leidy@gmail.com>
2025-02-06 15:46:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 //↓↓↓↓↓↓↓↓↓↓↓↓YOUR CODE goes below here↓↓↓↓↓↓↓↓↓↓↓↓
 const MIDIengine = class {
-    constructor (
-    input,
-    output   
-    ){
-    this.MIDIin = `input`;  
-    this.MIDIout = `output`;  
+  constructor(input, output) {
+    this.MIDIin = input;
+    this.MIDIout = output;
     this.running = false;
-    }   
-}
-enable(on_off) {
-this.running = on_off;
-console.log(`The MIDI enigine is now ${this.running `on` , `off`}.`);
-}
-printInfo(){
-console.log (`The MIDI engine has an input of ${this.MIDIin} and an output of ${this.MIDIout}`);   
-}
+  }
 
-
+  enable(on_off) {
+    this.running = on_off;
+    console.log(`The MIDI enigine is now ${this.running}.`);
+  }
+  printInfo() {
+    console.log(
+      `The MIDI engine has an input of ${this.MIDIin} and an output of ${this.MIDIout}`
+    );
+  }
+};
 
 //↑↑↑↑↑↑↑↑↑↑YOUR CODE goes above here↑↑↑↑↑↑↑↑↑↑
 //Do not edit below this comment!
-const testMIDI = new MIDIengine("MAudio", "PreSonus Interface")
-const testMIDI2 = new MIDIengine("OP-1","MOTU")
+const testMIDI = new MIDIengine("MAudio", "PreSonus Interface");
+const testMIDI2 = new MIDIengine("OP-1", "MOTU");
