18e06309899def798fce1ce2dfa5ad69f53c68a3
Merritt Leidy <merritt.leidy@gmail.com>
2025-02-06 13:17:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 //↓↓↓↓↓↓↓↓↓↓↓↓YOUR CODE goes below here↓↓↓↓↓↓↓↓↓↓↓↓
+const MIDIengine = class {
+    constructor (
+    input,
+    output   
+    ){
+    this.MIDIin = `input`;  
+    this.MIDIout = `output`;  
+    this.running = false;
+    }   
+}
+enable(on_off) {
+this.running = on_off;
+console.log(`The MIDI enigine is now ${this.running `on` , `off`}.`);
+}
+printInfo(){
+console.log (`The MIDI engine has an input of ${this.MIDIin} and an output of ${this.MIDIout}`);   
+}
 
 
 
